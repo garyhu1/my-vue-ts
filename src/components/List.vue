f7f8498e744d376714b0a94c578acc2b1e1bb8ee
huweiwei <huweiwei@superengine.com.cn>
2019-12-05 10:48:21 +0800

    展示请求数据列表

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <Button @click="changeParent" style="margin-bottom: 5px;">change</Button>
-        <Button @click="fetchData" style="margin-bottom: 5px;">拉取数据</Button>
+        <Button @click="fetchData" style="margin-bottom: 5px; margin-left: 8px;">拉取数据</Button>
+        <div v-if="isNodata">暂无数据</div>
+        <div style="margin: 10px 0;">
+          <div v-for="(item,index) in items" :key="index">{{ item.name }}</div>
+        </div>
     </div>
 </template>
 
@@ -10,7 +14,7 @@ import { Component, Vue} from 'vue-property-decorator'
 import Http from '../api'
 
 interface Resp<T> {
-  personal: T
+  items: T[]
 }
 
 interface PP {
@@ -20,13 +24,18 @@ interface PP {
 @Component({})
 export default class List extends Vue {
 
+  private isNodata: boolean = true;
+  private items: PP[] = [];
+
   changeParent(): void {
     this.$mybus.$emit("change");
   }
 
   fetchData(): void {
-    new Http().getUser().then((data) => {
+    new Http().getUser().then((data: any) => {
       console.log(data);
+      this.isNodata = false;
+      this.items = data.items;
     });
   }
 }
