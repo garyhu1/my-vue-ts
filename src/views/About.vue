1a55c0c6ad29a1e51ac1265cc18738b94143f316
huweiwei <huweiwei@superengine.com.cn>
2019-12-05 10:45:05 +0800

    添加自定义指令

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-colorDirective="{color: '#FFF', backgroundColor: '#DE9531'}">{{ message }}</div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import colorDirective from '../directive/color-directive'
+
+@Component({
+  directives: {
+    colorDirective
+  }
+})
+export default class About extends Vue {
+  private message: string = 'this is about vue-ts'
+}
+</script>
